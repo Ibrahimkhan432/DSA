cb63645aa6e99b4c3adfca3d350cce313a698b31
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-11-03 23:49:14 +0500

    switch case multi cases

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
- //--- type coercion
-console.log("2" + 2); // 22
-console.log(2 + "2"); // 22
-console.log("2" - 2); //0    
+//--- type coercion
+// console.log("2" + 2); // 22
+// console.log(2 + "2"); // 22
+// console.log("2" - 2); //0
+
+// ---switch case. multi cases
+let age = 10;
+switch (age) {
+  case 5:
+    console.log("less than 10", age);
+    break;
+  case 6:
+  case 7:
+  case 10:
+    console.log("equal to 10");
+    break;
+  default:
+    console.log("invalid value");
+}
