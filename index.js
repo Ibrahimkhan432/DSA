1e12add927203d28044f37e3d0a9d5839c2375f3
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-11-27 00:12:57 +0500

    right triangle

--- index.js
+++ index.js
@@ -78,3 +78,17 @@
 //   }
 //   console.log();
 // }
+
+//    ---RIGHT TRIANGLE
+// let i = 1; 
+// let j = 1;
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();  // right traingle
+// }
+// * 
+// * * 
+// * * * 
+// * * * * 
